208f57349e2c9a827b9fbdb08a47c71393fa52af
Salikh Magomedov <salikh.magomedov@icloud.com>
2020-10-06 16:42:35 +0300

    Dialogue Scriptable Objects

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Dialogue
+{
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject
+    {
+        [SerializeField] private DialogueNode[] nodes;
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,10 @@
+﻿namespace Dialogue
+{
+    [System.Serializable]
+    public class DialogueNode
+    {
+        public string uniqueId;
+        public string text;
+        public string[] children;
+    }
+}
