d5317a13e24801d2b0988fa4ba2409e0f8dc2f0c
Salikh Magomedov <salikh.magomedov@icloud.com>
2020-12-21 17:41:30 +0300

    Dialogue Architecture

---
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField] private Dialogue currentDialogue;
+
+        public string Text => currentDialogue == null ? "" : currentDialogue.GetRootNode().Text;
+    }
+}
---
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,22 @@
+﻿using RPG.Dialogue;
+using TMPro;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        private PlayerConversant _playerConversant;
+        [SerializeField] private TextMeshProUGUI aiText;
+
+        private void Awake()
+        {
+            _playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+        }
+
+        private void Start()
+        {
+            aiText.text = _playerConversant.Text;
+        }
+    }
+}
