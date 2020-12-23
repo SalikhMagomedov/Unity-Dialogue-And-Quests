e651ef2e2fee79935193ab844bf5699a06d5d411
Salikh Magomedov <salikh.magomedov@icloud.com>
2020-12-23 16:43:45 +0300

    Enemy Aggro Groups

---
+++ Assets/Scripts/Combat/AggroGroup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class AggroGroup : MonoBehaviour
+    {
+        [SerializeField] private Fighter[] fighters;
+        [SerializeField] private bool activateOnStart;
+        
+        private void Start()
+        {
+            Activate(activateOnStart);
+        }
+
+        public void Activate(bool shouldActivate)
+        {
+            foreach (var fighter in fighters)
+            {
+                var target = fighter.GetComponent<CombatTarget>();
+                if (target != null)
+                {
+                    target.enabled = shouldActivate;
+                }
+                fighter.enabled = shouldActivate;
+            }
+        }
+    }
+}
