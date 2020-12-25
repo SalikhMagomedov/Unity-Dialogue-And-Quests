e61f3b4d39c768dc944e18286014bf292b649c92
Salikh Magomedov <salikh.magomedov@icloud.com>
2020-12-25 11:19:02 +0300

    Tooltip Spawning & Show Hide

---
+++ Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -0,0 +1,18 @@
+﻿using GameDevTV.Core.UI.Tooltips;
+using UnityEngine;
+
+namespace RPG.UI.Quests
+{
+    public class QuestTooltipSpawner : TooltipSpawner
+    {
+        public override bool CanCreateTooltip()
+        {
+            return true;
+        }
+
+        public override void UpdateTooltip(GameObject tooltip)
+        {
+            
+        }
+    }
+}
