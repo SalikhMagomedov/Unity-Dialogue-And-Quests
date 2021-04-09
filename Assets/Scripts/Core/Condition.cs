6393131af5eef77bfe4b6a6db5c00fba69ff04ac
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-04-09 23:31:39 +0300

    Complex Conditions

--- Assets/Scripts/Core/Condition.cs
+++ Assets/Scripts/Core/Condition.cs
@@ -7,15 +7,38 @@ namespace RPG.Core
     [System.Serializable]
     public class Condition
     {
-        [SerializeField] private string predicate;
-        [SerializeField] private string[] parameters;
+        [SerializeField] private Disjunction[] and;
 
         public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
         {
-            return evaluators
-                .Select(evaluator => evaluator.Evaluate(predicate, parameters))
-                .Where(result => result != null)
-                .All(result => result != false);
+            return and.All(disjunction => disjunction.Check(evaluators));
+        }
+
+        [System.Serializable]
+        public class Disjunction
+        {
+            [SerializeField] private Predicate[] or;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                return or.Any(predicate => predicate.Check(evaluators));
+            }
+        }
+        
+        [System.Serializable]
+        private class Predicate
+        {
+            [SerializeField] private string predicate;
+            [SerializeField] private string[] parameters;
+            [SerializeField] private bool negate;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                return evaluators
+                    .Select(evaluator => evaluator.Evaluate(predicate, parameters))
+                    .Where(result => result != null)
+                    .All(result => result != negate);
+            }
         }
     }
 }
